af661e9fc7ddeea44f02e1822c4f4a66217ff180
Derek 💫 <arein@paypal.com>
2021-10-17 10:47:23 -0400

    misc

--- api/main.tf
+++ api/main.tf
@@ -33,38 +33,38 @@ resource "aws_s3_bucket" "lambda_bucket" {
   force_destroy = true
 }
 
-data "archive_file" "lambda_hello_world" {
+data "archive_file" "lambda_wallet_pass_generator" {
   type = "zip"
 
-  source_dir  = "${path.module}/hello-world"
-  output_path = "${path.module}/hello-world.zip"
+  source_dir  = "${path.module}/wallet-pass-generator"
+  output_path = "${path.module}/wallet-pass-generator.zip"
 }
 
-resource "aws_s3_bucket_object" "lambda_hello_world" {
+resource "aws_s3_bucket_object" "lambda_wallet_pass_generator" {
   bucket = aws_s3_bucket.lambda_bucket.id
 
-  key    = "hello-world.zip"
-  source = data.archive_file.lambda_hello_world.output_path
+  key    = "wallet-pass-generator.zip"
+  source = data.archive_file.lambda_wallet_pass_generator.output_path
 
-  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+  etag = filemd5(data.archive_file.lambda_wallet_pass_generator.output_path)
 }
 
-resource "aws_lambda_function" "hello_world" {
-  function_name = "HelloWorld"
+resource "aws_lambda_function" "wallet_pass_generator" {
+  function_name = "WalletPassGenerator"
 
   s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_bucket_object.lambda_hello_world.key
+  s3_key    = aws_s3_bucket_object.lambda_wallet_pass_generator.key
 
   runtime = "nodejs12.x"
-  handler = "hello.handler"
+  handler = "generator.handler"
 
-  source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
+  source_code_hash = data.archive_file.lambda_wallet_pass_generator.output_base64sha256
 
   role = aws_iam_role.lambda_exec.arn
 }
 
-resource "aws_cloudwatch_log_group" "hello_world" {
-  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+resource "aws_cloudwatch_log_group" "wallet_pass_generator" {
+  name = "/aws/lambda/${aws_lambda_function.wallet_pass_generator.function_name}"
 
   retention_in_days = 30
 }
@@ -121,19 +121,19 @@ resource "aws_apigatewayv2_stage" "lambda" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "hello_world" {
+resource "aws_apigatewayv2_integration" "wallet_pass_generator" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  integration_uri    = aws_lambda_function.hello_world.invoke_arn
+  integration_uri    = aws_lambda_function.wallet_pass_generator.invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
 
-resource "aws_apigatewayv2_route" "hello_world" {
+resource "aws_apigatewayv2_route" "wallet_pass_generator" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  route_key = "GET /hello"
-  target    = "integrations/${aws_apigatewayv2_integration.hello_world.id}"
+  route_key = "GET /generate"
+  target    = "integrations/${aws_apigatewayv2_integration.wallet_pass_generator.id}"
 }
 
 resource "aws_cloudwatch_log_group" "api_gw" {
@@ -145,7 +145,7 @@ resource "aws_cloudwatch_log_group" "api_gw" {
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
+  function_name = aws_lambda_function.wallet_pass_generator.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
