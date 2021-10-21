7be538370d4b32306348d5ae3eb7f26ef47afe44
Derek 💫 <alexanderderekrein@gmail.com>
2021-10-21 09:39:42 -0400

    adds logo

--- api/wallet-pass-generator/pass.js
+++ api/wallet-pass-generator/pass.js
@@ -19,22 +19,25 @@ const getPass = () => {
             template.setCertificate(fs.readFileSync(__dirname + "/keys/cert.pem").toString());
             template.setPrivateKey(fs.readFileSync(__dirname + "/keys/key.pem").toString(), passKeyPw);
             const pubKey = fs.readFileSync(__dirname + "/keys/encryptionPublicKey.pem").toString().replace("\n", "");
-            
-            template.images.add("icon", fs.readFileSync(__dirname + "/Event.pass/icon.png")).then((images) => {
-                template.images.add("logo", __dirname + "/Event.pass/logo.png").then((images) => {
-                    const pass = template.createPass({
-                        serialNumber: "123457",
-                        description: "Token Gate",
-                        nfc: {
-                            message: truncate("HelloRichard, my love <3", 64),
-                            encryptionPublicKey: pubKey
-                        }
-                    });
-                    pass.asBuffer().then((buffer) => {
-                        resolve(buffer);
-                    }).catch((err) => {
-                        reject(err);
-                    });
+
+            Promise.all([
+                template.images.add("icon", fs.readFileSync(__dirname + "/Event.pass/icon.png")),
+                template.images.add("logo", fs.readFileSync(__dirname + "/Event.pass/logo.png"), "2x"),
+                template.images.add("icon", fs.readFileSync(__dirname + "/Event.pass/icon@2x.png")),
+                template.images.add("logo", fs.readFileSync(__dirname + "/Event.pass/logo@2x.png"), "2x")
+            ]).then(() => {
+                const pass = template.createPass({
+                    serialNumber: "123457",
+                    description: "Token Gate",
+                    nfc: {
+                        message: truncate("HelloRichard, my love <3", 64),
+                        encryptionPublicKey: pubKey
+                    }
+                });
+                pass.asBuffer().then((buffer) => {
+                    resolve(buffer);
+                }).catch((err) => {
+                    reject(err);
                 });
             });
         } catch (err) {
