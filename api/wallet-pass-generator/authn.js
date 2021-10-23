802c429033715aef2ec189be6ff90556e1d155ce
Derek 💫 <alexanderderekrein@gmail.com>
2021-10-23 12:00:10 +0100

    misc

--- api/wallet-pass-generator/authn.js
+++ api/wallet-pass-generator/authn.js
@@ -5,14 +5,17 @@ var web3 = require('web3');
 const authn = (signature, publicAddress, balance) => {
     return new Promise((resolve, reject) => {
         const msg = `I own ${balance} tokens`;
+        console.log(msg);
 
         // We now are in possession of msg, publicAddress and signature. We
         // will use a helper from eth-sig-util to extract the address from the signature
         const msgBufferHex = ethUtil.bufferToHex(web3.utils.sha3(msg));
+        console.log(msgBufferHex);
         const address = ethSigUtil.recoverPersonalSignature({
             data: msgBufferHex,
             sig: signature,
         });
+        console.log(address);
     
         // The signature verification is successful if the address found with
         // sigUtil.recoverPersonalSignature matches the initial publicAddress
