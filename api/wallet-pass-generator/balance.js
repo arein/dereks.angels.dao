d319b9727c7a22ab624f786af8506807d779f284
Derek 💫 <alexanderderekrein@gmail.com>
2021-10-23 10:53:23 +0100

    misc

--- api/wallet-pass-generator/balance.js
+++ api/wallet-pass-generator/balance.js
@@ -1,6 +1,7 @@
 var Web3 = require('web3');
 var Web3Client = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/1c96603e022b4d97a08b70a95afae845"));
 var abi = require('./abi');
+var balance = require('./balance');
 
 
 const getBalance = (tokenAddress, walletAddress) => {
