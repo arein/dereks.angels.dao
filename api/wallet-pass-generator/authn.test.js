62d7fb3ca866c0bdbaf0232ed7898bda92481da8
Derek 💫 <alexanderderekrein@gmail.com>
2021-10-23 11:33:28 +0100

    misc

--- api/wallet-pass-generator/authn.test.js
+++ api/wallet-pass-generator/authn.test.js
@@ -1,11 +1,12 @@
-const assert = require('assert');
-const expect = require('chai');
 const authn = require('./authn');
 
 
 describe('Authn Test', () => {
     it('should login', (done) => {
-        authn.authn('0xbdd517dcf268ca74861d35fce8b9939274c8137485a6b0f9e7c283e3383a70e1046aa47093ec6e9d0c05a7409f456390bf6c293e6951e763ee4a41945a6f0c8e1b', '0xf3ea39310011333095CFCcCc7c4Ad74034CABA63', 990).then((isAuthed) => {
+        authn.authn(
+            '0xbdd517dcf268ca74861d35fce8b9939274c8137485a6b0f9e7c283e3383a70e1046aa47093ec6e9d0c05a7409f456390bf6c293e6951e763ee4a41945a6f0c8e1b',
+            '0xf3ea39310011333095CFCcCc7c4Ad74034CABA63', 990
+        ).then((isAuthed) => {
             console.log(isAuthed);
             done();
         }).catch((err) => {
